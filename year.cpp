928d48a1e3c2fbfa799b3428ed10aa207fd9663b
tanujapkamble2007-eng <tanujapkamble2007@gmail.com>
2026-07-27 21:18:25 +0530

    Create year.cpp

---
+++ year.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int year;
+    cout << "Enter a year: ";
+    cin >> year;
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) 
+        cout << "Leap year";
+     else 
+        cout <<"Not a Leap Year";
+      return 0;
+}
