fca2e993a4ee082824a9c87a6cb7f16587d3442a
tanujapkamble2007-eng <tanujapkamble2007@gmail.com>
2026-07-27 16:25:35 +0530

    Create arithmetic operations.cpp

---
+++ arithmetic operations.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"enter two numbers:";
+    cin>>a>>b;
+    cout<<"Addition="<<a+b<< endl;
+    cout<<"Subtraction="<<a-b<< endl;
+    cout<<"Multiplication="<<a*b<< endl;
+    cout<<"Division="<<a/b<< endl;
+    cout<<"modulus="<<a%b<< endl;
+    return 0;
+}
