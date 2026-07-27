feddf42ed731ddc8f2c5c9d038d5b13feca2f890
tanujapkamble2007-eng <tanujapkamble2007@gmail.com>
2026-07-27 21:05:29 +0530

    Create largest of two numbers.cpp

---
+++ largest of two numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout << "Enter two numbers: ";
+    cin >>a>>b;
+    if (a>b) 
+        cout <<a<< "is largest";
+     else 
+        cout<<b<<"is largest";
+      return 0;
+}
