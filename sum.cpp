627cb5cdeb8688e1320ca16cc878b3e01e951651
tanujapkamble2007-eng <tanujapkamble2007@gmail.com>
2026-08-03 19:28:56 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+
+    cout << "Sum of first " << n << " natural numbers = " << sum;
+
+    return 0;
+}
